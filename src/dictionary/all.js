03b029bab10320840467d14eddacbfde30dcb766
Arthur Fücher <arthurfucher@gmail.com>
2019-06-06 19:29:45 -0300

    added letter C

--- src/dictionary/all.js
+++ src/dictionary/all.js
@@ -1,5 +1,6 @@
 import a from './a.json';
 import b from './b.json';
+import c from './c.json';
 import md5 from 'md5';
 
 var all = {
@@ -7,8 +8,10 @@ var all = {
 }
 a.termos.map((termo)=>termo.id=md5(termo.title.toUpperCase()));
 b.termos.map((termo)=>termo.id=md5(termo.title.toUpperCase()));
+c.termos.map((termo)=>termo.id=md5(termo.title.toUpperCase()));
 
 a.termos.forEach((termo)=>all.termos.push(termo));
 b.termos.forEach((termo)=>all.termos.push(termo));
+c.termos.forEach((termo)=>all.termos.push(termo));
 
 export default all;
