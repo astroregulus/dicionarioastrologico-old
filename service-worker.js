d68872bd42103ddff08e079bdf34b47774247142
Arthur Fücher <arthurfucher@gmail.com>
2019-07-25 13:57:52 -0300

    changed service worker to cache then update and refresh

--- service-worker.js
+++ service-worker.js
@@ -29,29 +29,58 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-self.addEventListener('install', function(event){
-  console.log('[SW] Install');
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return Promise.all(
-      	files.map(function(file){
-      		return cache.add(file);
-      	})
-      );
-    })
+
+
+self.addEventListener('install', function(evt) {
+  // Open a cache and use `addAll()` with an array of assets to add all of them
+  // to the cache. Ask the service worker to keep installing until the
+  // returning promise resolves.
+  evt.waitUntil(caches.open(CACHE_NAME).then(function (cache) {
+    cache.addAll(files);
+  }));
+});
+
+// On fetch, use cache but update the entry with the latest contents
+// from the server.
+self.addEventListener('fetch', function(evt) {
+  // You can use `respondWith()` to answer ASAP...
+  evt.respondWith(fromCache(evt.request));
+  // ...and `waitUntil()` to prevent the worker to be killed until
+  // the cache is updated.
+  evt.waitUntil(
+    update(evt.request)
+    // Finally, send a message to the client to inform it about the
+    // resource is up to date.
+    .then(refresh)
   );
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('[SW] fetch ' + event.request.url)
-  event.respondWith(
-    caches.match(event.request).then(function(response){
-      return response || fetch(event.request.clone());
-    })
-  );
-});
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
 
-self.addEventListener('notificationclick', function(event) {
-  console.log('On notification click: ', event);
-  clients.openWindow('/');
-});
+// Sends a message to the clients.
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      // Encode which resource has been updated. By including the
+      // [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) the client can
+      // check if the content has changed.
+      var message = {
+        type: 'refresh',
+        url: response.url
+      };
+      console.log(message);
+      // Tell the client about the update.
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+}
